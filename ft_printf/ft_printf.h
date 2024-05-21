0b2d076655364300d5503715e649e4e8dc622db4
eduaserr <157403329+eduaserr@users.noreply.github.com>
2024-05-21 21:36:11 +0200

    added iniciated ft_printf

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eduaserr <eduaserr@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/21 20:38:00 by eduaserr          #+#    #+#             */
+/*   Updated: 2024/05/21 20:41:29 by eduaserr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+
+/**
+ * @param str 
+ * @return (Int) 
+*/
+int		ft_printf(char const *str, ...);
+
+#endif
